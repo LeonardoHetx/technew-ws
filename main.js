2181e75feefadc29e448568f8df2b086d11d6868
Leonardo Henrique Texeira <leonardohetx@gmail.com>
2022-05-30 16:03:33 -0300

    JS Code

--- main.js
+++ main.js
@@ -5,24 +5,40 @@ onScroll()
 function onScroll() {
    showNavOnScroll()
    showBackToTopButtonOnScroll()
+
    activateMenuAtCurrentSection(home)
+   activateMenuAtCurrentSection(services)
+   activateMenuAtCurrentSection(about)
+   activateMenuAtCurrentSection(contact)
 }
 
-function activateMenuAtCurrentSection(section){
+function activateMenuAtCurrentSection(section) {
    const targetLine = scrollY + innerHeight / 2
+
+   //verificar se a seção passou da linha
    const sectionTop = section.offsetTop
    const sectionHeight = section.offsetHeight
-
    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
-   console.log(sectionTopReachOrPassedTargetLine)
-   console.log(targetLine)
-   console.log(sectionTop)
 
+   //verificar se a base está abaixo da linha alvo
    const sectionEndsAt = sectionTop + sectionHeight
-   console.log(sectionEndsAt)
+   const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+   //limites da seção
+   const sectionBoundaries =
+      sectionTopReachOrPassedTargetLine &&
+      !sectionEndPassedTargetLine
+
+   const sectionId = section.getAttribute(`id`)
+   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+   menuElement.classList.remove('active')
+   if (sectionBoundaries) {
+      menuElement.classList.add('active')
+   }
 }
 
-function showNavOnScroll(){
+function showNavOnScroll() {
    if (scrollY > 0) {
       navigation.classList.add('scroll')
    } else {
@@ -30,7 +46,7 @@ function showNavOnScroll(){
    }
 }
 
-function showBackToTopButtonOnScroll(){
+function showBackToTopButtonOnScroll() {
    if (scrollY > 1400) {
       backToTopButton.classList.add('show')
    } else {
