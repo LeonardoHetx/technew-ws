228615bd638054ee7f82d5078305d78928af2e99
Leonardo Henrique Texeira <leonardohetx@gmail.com>
2022-06-03 15:17:59 -0300

    phase1 slider

--- main.js
+++ main.js
@@ -83,10 +83,4 @@ ScrollReveal({
 
    //SLIDER SECTION#ABOUT
 
-   document.querySelector('.items').addEventListener("wheel", event => {
-      if(event.deltaY > 0) {
-         event.target.scrollBy(300, 0)
-      } else {
-         event.target.scrollBy(-300, 0)
-      }
-   })
+ 
